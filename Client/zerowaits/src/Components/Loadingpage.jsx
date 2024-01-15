e53e0da4c9b4af0ff529a4a996f12752e81af57d
karani-ken <academice79@gmail.com>
2024-01-15 11:08:57 +0300

    Done with the welcome page

---
+++ Client/zerowaits/src/Components/Loadingpage.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './components.css'
+import WelcomeImage from '../Assets/Images/Welcome.png'
+function Loadingpage() {
+    return (
+        <div className='welcome-page'>
+
+            <div className='main-text'>
+                <h1>ZeroWaits</h1>
+                <p>Order in a flash, Delivering joy in a Dash!</p>
+
+                <div className='welcome-image'>
+                    <img src={WelcomeImage} alt="" />
+                </div>
+            </div>
+
+
+        </div>
+    )
+}
+
+export default Loadingpage
